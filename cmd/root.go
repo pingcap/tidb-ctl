65972f778cabcb9318645e61554f84a13c94c859
lysu <sulifx@gmail.com>
2020-03-10 17:00:09 +0800

    support tls in tidb-ctl

--- cmd/root.go
+++ cmd/root.go
@@ -15,6 +15,8 @@ package cmd
 
 import (
 	"bytes"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -23,17 +25,23 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 )
 
 // root command flags
 var (
-	host   net.IP
-	port   uint16
-	genDoc bool
-	pdHost net.IP
-	pdPort uint16
+	host      net.IP
+	port      uint16
+	genDoc    bool
+	pdHost    net.IP
+	pdPort    uint16
+	ca        string
+	sslCert   string
+	sslKey    string
+	ctlClient *http.Client
+	schema    string
 )
 
 const (
@@ -81,8 +89,8 @@ func Execute() {
 }
 
 func httpPrint(path string) error {
-	url := "http://" + host.String() + ":" + strconv.Itoa(int(port)) + "/" + path
-	resp, err := http.Get(url)
+	url := schema + "://" + host.String() + ":" + strconv.Itoa(int(port)) + "/" + path
+	resp, err := ctlClient.Get(url)
 	if err != nil {
 		return err
 	}
@@ -109,22 +117,90 @@ func httpPrint(path string) error {
 	return nil
 }
 
-func init() {
-	hostFlagName := "host"
-	portFlagName := "port"
-	docFlagName := "doc"
-	pdHostFlagName := "pdhost"
-	pdPortFlagName := "pdport"
+const (
+	hostFlagName   = "host"
+	portFlagName   = "port"
+	docFlagName    = "doc"
+	pdHostFlagName = "pdhost"
+	pdPortFlagName = "pdport"
+	caName         = "ca"
+	sslKeyName     = "ssl-key"
+	sslCertName    = "ssl-cert"
+)
 
+func init() {
 	rootCmd.AddCommand(mvccRootCmd, schemaRootCmd, regionRootCmd, tableRootCmd, newBase64decodeCmd, decoderCmd, logCmd, newEtcdCommand())
 
 	rootCmd.PersistentFlags().IPVarP(&host, hostFlagName, "", net.ParseIP("127.0.0.1"), "TiDB server host")
 	rootCmd.PersistentFlags().Uint16VarP(&port, portFlagName, "", 10080, "TiDB server port")
 	rootCmd.PersistentFlags().IPVarP(&pdHost, pdHostFlagName, "", net.ParseIP("127.0.0.1"), "PD server host")
 	rootCmd.PersistentFlags().Uint16VarP(&pdPort, pdPortFlagName, "", 2379, "PD server port")
+	rootCmd.PersistentFlags().StringVarP(&ca, caName, "", "", "TLS CA path")
+	rootCmd.PersistentFlags().StringVarP(&sslKey, sslKeyName, "", "", "TLS Key path")
+	rootCmd.PersistentFlags().StringVarP(&sslCert, sslCertName, "", "", "TLS Cert path")
 	rootCmd.Flags().BoolVar(&genDoc, docFlagName, false, "generate doc file")
 	if err := rootCmd.Flags().MarkHidden(docFlagName); err != nil {
 		fmt.Printf("can not mark hidden flag, flag %s is not found", docFlagName)
 		return
 	}
+	cobra.OnInitialize(func() {
+		tlsConfig, err := prepareTlsConfig()
+		if err != nil {
+			fmt.Printf("cannot setup tls: %v", err)
+		}
+		if tlsConfig != nil {
+			schema = "https"
+		} else {
+			schema = "http"
+		}
+		ctlClient = &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: tlsConfig,
+			},
+		}
+	})
+}
+
+func prepareTlsConfig() (tlsConfig *tls.Config, err error) {
+	if len(ca) != 0 {
+		tlsConfig = &tls.Config{}
+		certPool := x509.NewCertPool()
+		// Create a certificate pool from the certificate authority
+		var caBytes []byte
+		caBytes, err = ioutil.ReadFile(ca)
+		if err != nil {
+			err = errors.Errorf("could not read ca certificate: %s", err)
+			return
+		}
+		// Append the certificates from the CA
+		if !certPool.AppendCertsFromPEM(caBytes) {
+			err = errors.New("failed to append ca certs")
+			return
+		}
+		tlsConfig.RootCAs = certPool
+	}
+	if len(sslCert) != 0 && len(sslKey) != 0 {
+		if tlsConfig == nil {
+			tlsConfig = &tls.Config{}
+		}
+		getCert := func() (*tls.Certificate, error) {
+			// Load the client certificates from disk
+			cert, err := tls.LoadX509KeyPair(sslCert, sslKey)
+			if err != nil {
+				return nil, errors.Errorf("could not load client key pair: %s", err)
+			}
+			return &cert, nil
+		}
+		// pre-test cert's loading.
+		if _, err = getCert(); err != nil {
+			return
+		}
+		tlsConfig.GetClientCertificate = func(info *tls.CertificateRequestInfo) (certificate *tls.Certificate, err error) {
+			return getCert()
+		}
+		tlsConfig.GetCertificate = func(info *tls.ClientHelloInfo) (certificate *tls.Certificate, err error) {
+			return getCert()
+		}
+	}
+	return
 }
